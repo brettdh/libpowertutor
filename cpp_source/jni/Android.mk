bb46a6a1f132fe6b883632f367b3f8335b27007b
Brett Higgins <brettdh@eecs.umich.edu>
2012-10-24 11:18:09 -0400

    Tweaks for new build on Android.

--- cpp_source/jni/Android.mk
+++ cpp_source/jni/Android.mk
@@ -12,7 +12,7 @@ MY_CFLAGS := -g -ggdb -O0 -Wall -Werror -DANDROID
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libmocktime
-LOCAL_SRC_FILES := ../../../mocktime/obj/local/armeabi/libmocktime.so
+LOCAL_SRC_FILES := ../../../mocktime/obj/local/$(TARGET_ARCH_ABI)/libmocktime.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 # Use the static lib if there happen to be exception-related
@@ -24,6 +24,6 @@ LOCAL_C_INCLUDES := $(ANDROID_INCLUDES)
 LOCAL_SRC_FILES := $(addprefix ../, $(MY_SRCS))
 LOCAL_CFLAGS := $(MY_CFLAGS) -DBUILDING_SHLIB
 LOCAL_SHARED_LIBRARIES := liblog libmocktime
-LOCAL_LDLIBS := -L./obj/local/armeabi -llog -lmocktime
+LOCAL_LDLIBS := -llog
 LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
