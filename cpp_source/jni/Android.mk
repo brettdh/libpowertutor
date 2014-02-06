8da05d54ed2540831e957813e9f8ce14c3d39f53
Brett Higgins <brett.higgins@gmail.com>
2014-02-06 16:36:16 -0500

    Removed unnecessary android-source deps.

--- cpp_source/jni/Android.mk
+++ cpp_source/jni/Android.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 MY_ANDROID_SRC_ROOT := $(HOME)/src/android-source
-ANDROID_INCLUDES := $(MY_ANDROID_SRC_ROOT)/system/core/include
-ANDROID_INCLUDES += $(MY_ANDROID_SRC_ROOT)/external/wpa_supplicant
+#ANDROID_INCLUDES := $(MY_ANDROID_SRC_ROOT)/system/core/include
+#ANDROID_INCLUDES += $(MY_ANDROID_SRC_ROOT)/external/wpa_supplicant
 ANDROID_INCLUDES += $(HOME)/src/android-ndk-r8b/sources/
 ANDROID_INCLUDES += $(MY_ANDROID_SRC_ROOT)/external/bdh_apps/mocktime/
 
 MY_SRCS := libpowertutor.cpp power_model.cpp timeops.cpp utils.cpp jni_wrappers.cpp debug.cpp
-MY_CFLAGS := -g -ggdb -O0 -Wall -Werror -DANDROID -std=c++11
+MY_CFLAGS := -g -ggdb -O0 -Wall -Werror -DANDROID -DNDK_BUILD -std=c++11
 
 #include $(CLEAR_VARS)
 
