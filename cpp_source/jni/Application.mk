3f6c3dccbfbb2d76a80667c7be1deb0baa90cb5f
Brett Higgins <brett.higgins@gmail.com>
2014-02-25 13:45:49 -0500

    Updated build to match other projects.

--- cpp_source/jni/Application.mk
+++ cpp_source/jni/Application.mk
@@ -1,4 +1,5 @@
-APP_CFLAGS := -fexceptions
-APP_STL := gnustl_static
+APP_CFLAGS := -fexceptions -frtti
+APP_STL := gnustl_shared
 APP_ABI := armeabi-v7a
 NDK_TOOLCHAIN := arm-linux-androideabi-4.7
+APP_PLATFORM := android-9
